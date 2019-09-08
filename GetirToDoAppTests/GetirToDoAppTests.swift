eff0c7613854ffc5bbe878c5c82249e94cbbc330
Töre Bayındır <tore@betbull.com>
2019-09-08 15:42:46 +0300

    Initial Commit

---
+++ GetirToDoAppTests/GetirToDoAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  GetirToDoAppTests.swift
+//  GetirToDoAppTests
+//
+//  Created by Töre Bayındır on 8.09.2019.
+//  Copyright © 2019 Töre Bayındır. All rights reserved.
+//
+
+import XCTest
+@testable import GetirToDoApp
+
+class GetirToDoAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
